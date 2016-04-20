cb71b9481bfee0df00c6661ddd8d507084e0207f
mabbadi <mabbadi@outlook.com>
2016-04-20 17:29:37 +0200

    Updated slides + visitor samples

---
+++ Design Patterns Samples C#/Lesson 1/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionNoLambda
+{
+  interface IOptionVisitor<T, U>
+  {
+    U Visit(Some<T, U> option);
+    U Visit(None<T, U> option);
+  }
+  class IncOptionVisitor<T, U> : IOptionVisitor<T, U>
+  {
+    Func<T, U> onSome;
+    Func<U> onNone;
+    public IncOptionVisitor(Func<T, U> onSome, Func<U> onNone) {
+      this.onSome = onSome;
+      this.onNone = onNone;
+    }
+
+    public U Visit(None<T, U> option)
+    {
+      return onNone();
+    }
+
+    public U Visit(Some<T, U> option)
+    {
+      return onSome(option.value);
+    }
+
+  }
+  interface Option<T, U> { U Accept(IncOptionVisitor<T, U> visitor); }
+  class Some<T, U> : Option<T, U>
+  {
+    public T value;
+    public Some(T value) { this.value = value; }
+    public U Accept(IncOptionVisitor<T, U> visitor)
+    {
+      return visitor.Visit(this);
+    }
+  }
+  class None<T, U> : Option<T, U>
+  {
+    public U Accept(IncOptionVisitor<T, U> visitor)
+    {
+      return visitor.Visit(this);
+    }
+  }
+}
+namespace OptionLambda
+{
+  interface Option<T>
+  {
+    U Visit<U>(Func<U> onNone, Func<T, U> onSome);
+  }
+  class Some<T> : Option<T>
+  {
+    T value;
+    public Some(T value) { this.value = value; }
+    public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+    {
+      return onSome(value);
+    }
+  }
+  class None<T> : Option<T>
+  {
+    public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+    {
+      return onNone();
+    }
+  }
+}
+namespace Number
+{ 
+  interface INumberVisitor
+  {
+    void Visit(MyFloat number);
+    void Visit(MyInt number);
+  }
+  class NumberVisitor : INumberVisitor
+  {
+    public void Visit(MyFloat number) { Console.WriteLine("Found a float and now?"); }
+    public void Visit(MyInt number) { Console.WriteLine("Found an int and now?!"); }
+  }
+  interface Number { void Visit(INumberVisitor visitor); }
+  class MyInt : Number
+  {
+    void Number.Visit(INumberVisitor visitor)
+    {
+      visitor.Visit(this);
+    }
+  }
+  class MyFloat : Number
+  {
+    void Number.Visit(INumberVisitor visitor)
+    {
+      visitor.Visit(this);
+    }
+  }
+
+}
+namespace MusicLibrary
+{
+  interface IMusicLibraryVisitor
+  {
+    void Visit(HeavyMetal number);
+    void Visit(Jazz number);
+  }
+  class MusicLibraryVisitor : IMusicLibraryVisitor
+  {
+    public List<HeavyMetal> heavyMetal = new List<HeavyMetal>();
+    public List<Jazz> jazz = new List<Jazz>();
+
+    public void Visit(HeavyMetal song) { heavyMetal.Add(song); }
+    public void Visit(Jazz song) { jazz.Add(song); }
+  }
+  interface Song { void Visit(IMusicLibraryVisitor visitor); }
+  class HeavyMetal : Song
+  {
+    string title;
+    public HeavyMetal(string title) { this.title = title; }
+    public void Visit(IMusicLibraryVisitor visitor)
+    {
+      visitor.Visit(this);
+    }
+  }
+  class Jazz : Song
+  {
+    string title;
+    public Jazz(string title) { this.title = title; }
+    public void Visit(IMusicLibraryVisitor visitor)
+    {
+      visitor.Visit(this);
+    }
+  }
+}
+
+namespace ConsoleApplication1
+{
+  using Number;
+  using OptionNoLambda;
+  using MusicLibrary;
+  using OptionLambda;
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      //NumberVisitor n_visitor = new NumberVisitor();
+      //Number n = new MyInt();
+      //n.Visit(n_visitor);
+
+      //MusicLibraryVisitor music_library_visitor = new MusicLibraryVisitor();
+      //List<Song> songs = new List<Song>();
+      //songs.Add(new HeavyMetal("Hallowed Be Thy Name"));
+      //songs.Add(new Jazz("Autumn Leaves"));
+      //songs.Add(new HeavyMetal("War Pigs"));
+      //foreach (var song in songs) { song.Visit(music_library_visitor); }
+      //Console.WriteLine("Amount of heavy metal music: " + music_library_visitor.heavyMetal.Count);
+      //Console.WriteLine("Amount of jazz music: " + music_library_visitor.jazz.Count);
+
+      //OPTION VISITOR version 1
+      //IncOptionVisitor<int, int> opt_visitor = new IncOptionVisitor<int, int>(i => i + 1, () => { throw new Exception("Expexting a value.."); });
+      //Option<int, int> opt = new Some<int, int>(5);
+      //int res = opt.Accept(opt_visitor);
+      //Console.WriteLine(res);
+
+      //OPTION VISITOR version 2
+      Option<int> number = new Some<int>(5);
+      int inc_number = number.Visit(() => { throw new Exception("Expexting a value.."); }, i => i + 1);
+      Console.WriteLine(inc_number);
+      number = new None<int>();
+      inc_number = number.Visit(() => { throw new Exception("Expexting a value.."); }, i => i + 1);
+      Console.WriteLine(inc_number);
+
+
+
+
+    }
+  }
+}
--- Slides/Chapter1_Week1v2.fs
+++ Slides/Chapter1_Week1v2.fs
@@ -128,7 +128,14 @@ O\left(\sum_{\substack{
         ! @"Given two classes \texttt{A} and \texttt{B};"
         ! @"\texttt{A} interacts with an \texttt{I\_B} interface, whenever \texttt{A} needs to interact with an instance of type \texttt{B};"
         ! @"\texttt{B} interacts with an \texttt{I\_A} interface, whenever \texttt{B} needs to interact with an instance of type \texttt{A}."
-        ! @"UML MISSING"
+      ]
+    UML
+      [ Interface("IA",3.0,-3.0,0.0,[])
+        Interface("IB",3.0,3.0,0.0,[])
+        Class("A", -3.0, -3.0, Some "IA", [], [])
+        Class("B", 3.0, -3.0, Some "IB", [], [])
+        Aggregation("A","ib",1,"IB")
+        Aggregation("B","ia",1,"IA")
       ]
     SubSection("Polymorphism for taming coupling in programs")
     ItemsBlock
@@ -212,25 +219,132 @@ O\left(\sum_{\substack{
     SubSection("Safe choice in the presence of polymorphism")
     ItemsBlock
       [
-        ! @"We need a mechanism that allows us to manipulate polymorphic instances as id they were concrete;"
+        ! @"We need a mechanism that allows us to manipulate polymorphic instances as if they were concrete;"
         ! @"Concrete instances are the only ones who know their identity, so we allow them to choose from a series of given ``options''."        
       ]
+        
+    Section("Visiting Option's")
+    SubSection("The Option data structure")
+    ItemsBlock
+      [
+        ! @"Is used when an actual value might not exist for a named value or variable;" 
+        ! @"An option has an underlying type and can hold a value of that type, or it might not have a value."
+      ]
+    SubSection("Example of usage")
+    ItemsBlock
+      [
+        ! @"The following code illustrates the use of the option type;"
+        ! @"In this case we are capturing the number \texttt{5} within a \texttt{Some<int>} object;"
+        ! @"[] \begin{lstlisting}
+Option<int> a_number = new Some<int>(5);
+\end{lstlisting}"   
+        ! @"In this case we captring the ``nothing'' common to all values of type \texttt{int} withing a \texttt{None<int>} object;"
+        ! @"[] \begin{lstlisting}
+Option<int> another_number = new None<int>();
+\end{lstlisting}" 
+      ]
 
-    Section("Visiting Option's without lambda's")
+    SubSection("Some<T> and None<T>")
     ItemsBlock
       [
-        ! @"UML + Examplanation"
+        ! @"Both types implement the \texttt{Option<T>} data structure;"
+        ! @"[] \begin{lstlisting}
+class Some<T> : Option<T> { ... }
+\end{lstlisting}" 
+        ! @"[] \begin{lstlisting}
+class None<T> : Option<T> { ... }
+\end{lstlisting}" 
+        ! @"\texttt{Some<T>} is a container of data, of type \texttt{T}, which is ready to get consumed; and"
+        ! @"\texttt{None<T>} is a container of data, of type \texttt{T}, which is not ready to get consumed yet."
       ]
-    Section("Visiting Option's with lambda's")
+
+    SubSection("Option<T>")
     ItemsBlock
       [
-        ! @"UML + Examplanation"
+        ! @"Is an interface that represents both the absence and presence of data of type \texttt{T}"
+        ! @"[] \begin{lstlisting}
+interface Option<T> { ... }
+\end{lstlisting}" 
       ]
+
+    SubSection("Visiting an Option<T>")
+    ItemsBlock
+      [
+        ! @"As option represents a generic container for any type of objects, we need a mechanism that allows us to manipulate its content regardless its concrete data type;"
+        ! @"We add a method to our interface called \texttt{Visit} that accepts as inputs a series of options (in the shape of lambdas) and a generic result;"
+        ! @"Each option will be selected by exactly one of the possible concrete types;"
+        ! @"We decided a propri that the first argument is meant for the class \texttt{None<T>} while the second one for the \texttt{Some<T>}"
+        ! @"[] \begin{lstlisting}
+interface Option<T>
+{
+  U Visit<U>(Func<U> onNone, Func<T, U> onSome);
+}
+\end{lstlisting}" 
+      ]
+
+    SubSection("Visiting a None<T>")
+    ItemsBlock
+      [
+        ! @"When visiting an object of type \texttt{None<T>} we first select the input reserved for it then we return the result of its call;"
+        ! @"[] \begin{lstlisting}
+public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+{
+  return onNone();
+}
+\end{lstlisting}" 
+      ]
+
+    SubSection("Visiting a Some<T>")
+    ItemsBlock
+      [
+        ! @"When instantiating a \texttt{Some<T>} a data of type \texttt{T} is passed and stored inside a field \texttt{value};"
+        ! @"When visiting an object of type \texttt{Some<T>} we first select the input reserved for it then we return the result of its call with \texttt{value} given as input;"
+        ! @"We pass \texttt{value} to the lambda, since it might be transformed/consumed by it;"        
+        ! @"[] \begin{lstlisting}
+class Some<T> : Option<T>
+{
+  T value;
+  public Some(T value) { this.value = value; }
+  public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+  {
+    return onSome(value);
+  }
+}
+\end{lstlisting}" 
+      ]
+    SubSection("Testing out our Option<T>")
+    ItemsBlock
+      [
+        ! @"The next line shows how to use our option to capture numbers and define operations over it;"
+        ! @"More precisely we define a \texttt{Some} containing the number 5 with the following operations: \begin{itemize} \item The first lambda runs an exception, since we are trying to read a data that is not ready (None represents a \texttt{null} object); \item The second lambda gets as input the \texttt{value} stored into \texttt{Some} and increments it by 1. \end{itemize} "
+        ! @"[] \begin{lstlisting}
+Option<int> number = new Some<int>(5);
+int inc_number = number.Visit(() => { throw new Exception(\""Expexting a value..\""); }, i => i + 1);
+\end{lstlisting}" 
+      ]
+    SubSection("Testing out our Option<T>")
+    ItemsBlock
+      [
+        ! @"The next line shows an example with a \texttt{None} object;"
+        ! @"Visiting such object will indeed cause an exception;"
+        ! @"[] \begin{lstlisting}
+number = new None<int>();
+int inc_number = number.Visit(() => { throw new Exception(\""Expexting a value..\""); }, i => i + 1);
+\end{lstlisting}" 
+        ! @"As we see we managed to define operations on the fly over polimorphic data types in a controlled way;"
+        ! @"This design will work properly (regadless the data type captured by \texttt{T}) as long as there are always options to choose."
+      ]
+    SubSection("More sample")
+    ItemsBlock
+      [
+        ! @"Can be found on GIT under the folder: \texbf{Design Patterns Samples C#}."
+      ]
+
     Section("The visitor design pattern")
     SubSection("The general idea")
     ItemsBlock
       [
-        ! @"What we have seen so far are all examples implementing the \textit{visitor} design pattern;"                
+        ! @"What we have seen so far is an example implementing the \textit{visitor} design pattern;"                
         ! @"It allows the recovery of ``lost-type'' information from a general instance back to specifics;"
         ! @"The recovery is based on the actualy activation of one of the multiple ``options'';"
         ! @"The options can be instances of some concrete visitor interface, or (more elegantly) lambda's;"
@@ -254,27 +368,27 @@ O\left(\sum_{\substack{
         ! @"The \texttt{Visit} method accepts as input arguments as many as the possible concrete classes;"
         ! @"Every argument is a function that depends on the fields of the concrete instance and produces a result of type \texttt{U}."
         ! @"[] \begin{lstlisting} 
-interface I<Input_1, Input_2, ..., Input_N>
+interface I<$FieldsC_1$, $FieldsC_2$, ..., $FieldsC_N$>
   { 
-    U Visit<U>(Func<Input_1, U> onObj1, 
+    U Visit<U>(Func<$FieldsC_1$, U> $onC_1$, 
                ..., 
-               Func<Input_N, U> onObjN);
+               Func<$FieldsC_N$, U> $onC_N$);
   }
 \end{lstlisting}"
       ]
     SubSection("How do we implement it (lambda version)? (Step 3)")
     ItemsBlock
       [
-        ! @"Every class implementing the interface \texttt{M} has the task now to implement the \texttt{Visit} method, by selecting and calling the appropriate argument."
+        ! @"Every class implementing the interface \texttt{I} has the task now to implement the \texttt{Visit} method, by selecting and calling the appropriate argument."
         ! @"[] \begin{lstlisting} 
-class C1<Input_1, Input_2, ..., Input_N> 
-     : I<Input_1, Input_2, ..., Input_N>
+class C1<$FieldsC_1$, $FieldsC_2$, ..., $FieldsC_N$> 
+     : I<$FieldsC_1$, $FieldsC_2$, ..., $FieldsC_N$>
   { 
     Input_1 value;
-    U Visit<U>(Func<Input_1, U> onObj1, 
+    U Visit<U>(Func<$FieldsC_1$, U> $onC_1$, 
                ..., 
-               Func<Input_N, U> onObjN){
-      onObj1(this.value);
+               Func<$FieldsC_N$, U> $onC_N$){
+      $onC_1$(this.value);
     }
   }
 \end{lstlisting}"
@@ -284,7 +398,7 @@ class C1<Input_1, Input_2, ..., Input_N>
       [
         ! @"Every time we want to consume an instance of type \texttt{M} we have to \texttt{Visit} it."
         ! @"[] \begin{lstlisting} 
-I<Input_1, Input_2, ..., Input_N> i;
+I<$FieldsC_1$, $FieldsC_2$, ..., $FieldsC_N$> i;
 ...
 m.Visit(
  i_1 => $b_1$,
@@ -292,17 +406,17 @@ m.Visit(
  i_N => $b_n$);
 \end{lstlisting}" 
         ! @"Every argment of the visit becomes a function that is triggered depending on the concrete type of \texttt{i};"
-        ! @"\texttt{i_i} are the fields of a concrete class $C_i$};
-        ! @""\texttt{b_i} is the block of to run when a visit on an instance of type $C_i$ is needed."]
+        ! @"\texttt{i\_i} are the fields of a concrete class $C_i$};"
+        ! @"\texttt{b\_i} is the block of to run when a visit on an instance of a concrete type $C_i$ is needed."]
 
     SubSection("A visual representation")
     UML
       [ Package("Visitor", 
-                [Interface("M<Obj\_1, Obj\_N>",10.0,0.0,0.0,[Operation("Visit<U>", [@"onObj1 : Obj1 $\rightarrow$ U"; @"onObjN : ObjN $\rightarrow$ U"], Some "U")])
-                 Class("Obj1", -3.0, -3.0, Some "M<Obj\_1, Obj\_N>", [Attribute("value", "Obj\_1")], [Operation("Visit<U>", [@"onObj1 : Obj1 $\rightarrow$ U"; "..."], Some "U")])
-                 Class("ObjN", 3.0, -3.0, Some "M<Obj\_1, Obj\_N>", [Attribute("value", "Obj\_N")], [Operation("Visit<U>", ["..."; @"onObjN : ObjN $\rightarrow$ U"], Some "U")])])
-        Class("Program", 5.5, -7.5, Option.None, [], [Operation("main", [], Option.None)])
-        Arrow("Program", "Consumes", "Visitor")
+                [Interface("M<FieldsC1, FieldsCN>",10.0,0.0,0.0,[Operation("Visit<U>", [@"onC1 : FieldsC1 $\rightarrow$ U"; "..." ; @"onCN : FieldsCN $\rightarrow$ U"], Some "U")])
+                 Class("C1", -3.0, -3.0, Some "M<FieldsC1, FieldsCN>", [Attribute("value", "FieldsC1")], [Operation("Visit<U>", [@"onC1 : FieldsC1 $\rightarrow$ U"; "..."], Some "U")])
+                 Class("CN", 3.0, -3.0, Some "M<FieldsC1, FieldsCN>", [Attribute("value", "FieldsCN")], [Operation("Visit<U>", ["..."; @"onCN : FieldsCN $\rightarrow$ U"], Some "U")])
+                 ])
+
       ]
     SubSection("Final considerations")
     ItemsBlock
