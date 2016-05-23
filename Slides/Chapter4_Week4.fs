ecbde3d7b9d1aec3b05ed3938c89b188918409ad
mabbadi <mabbadi@outlook.com>
2016-05-23 12:10:29 +0200

    factory

--- Slides/Chapter4_Week4.fs
+++ Slides/Chapter4_Week4.fs
@@ -19,7 +19,7 @@ let slides (title : string) =
         ! @"Abstract factory"
         ! @"Conclusions"
       ]
-    Section("The factory design pattern")
+    Section("The problem")
     SubSection("Introduction")
     ItemsBlock
       [
@@ -41,12 +41,6 @@ let slides (title : string) =
         ! @"This design pattern is going to be the topic of this lecture"
       ]
 
-    SubSection "Motivations"
-    ItemsBlock
-      [
-        // not very clear
-        ! @"We wish to standardize our application across different domains, so to provide a unique way for instantiating entities"
-      ]
     SubSection "Our first example"
     VerticalStack
       [
@@ -86,11 +80,11 @@ let slides (title : string) =
                          typedDeclAndInit "input" "int" (constInt -1) >>
                          Code.While(Code.Op(var "input", Operator.NotEquals, constInt 0),
                                     ("input" := StaticMethodCall("Int32", "Parse", [(StaticMethodCall("Console", "ReadLine", []))])) >>
-                                    (Code.IfThen(Code.Op(var "input", Operator.NotEquals, constInt 1),
+                                    (Code.IfThen(Code.Op(var "input", Operator.Equals, constInt 1),
                                                   MethodCall("animals", "Add", [newC "Cat" []]))) >>
-                                    (Code.IfThen(Code.Op(var "input", Operator.NotEquals, constInt 2),
+                                    (Code.IfThen(Code.Op(var "input", Operator.Equals, constInt 2),
                                                   MethodCall("animals", "Add", [newC "Dog" []]))) >>
-                                    (Code.IfThen(Code.Op(var "input", Operator.NotEquals, constInt 3),
+                                    (Code.IfThen(Code.Op(var "input", Operator.Equals, constInt 3),
                                                   MethodCall("animals", "Add", [newC "Bird" []]))))))
       ]
     SubSection "Consuming our ``animals'': from different clients"
@@ -115,49 +109,218 @@ let slides (title : string) =
         ! @"How? "
         Pause
         ! @"By defining special methods that create and return concrete classes belonging to some polymorphic type"
+        ! @"Such special-purpose classes are called abstract classes"
       ]
-    
-    SubSection ("Upgrading our ``Animal'' to abstract class")
-    ItemsBlock
+    SubSection ("About abstract classes")
+    VerticalStack
       [
-        ! @"From interface to class"
-        ! @"In particular we define our new \texttt{Animal} abstract"
-        ! @"Abstract classes allow the declaration of classes that can contain both empty and not empty methods"
-        ! @"Indeed we can literally copy the signature of \texttt{MakeSound} in it"
+        ItemsBlockWithTitle ("About abstract classes") 
+          [
+            ! @"In OO programming it is possible to design special classes containing methods with or without bodies"
+            ! @"These special classes are called \textit{abstract}"
+            ! @"In the following an abstract class \texttt{Weapon} contains a concrete method \texttt{GetAmountOfBullets} and an abstract \texttt{Fire}"
+            ! @"\texttt{Fire} is abstract, since different weapons might come with different kinds of firing"
+          ]
+        CSharpCodeBlock(TextSize.Tiny, 
+                        (abstractClassDef "Weapon" 
+                                          [typedDecl "amounOfBullets" "int"|> makePublic
+                                           typedDef "Weapon" ["int","amounOfBullets"] "" ((("this.amounOfBullets" := var"amounOfBullets") >> endProgram)) |> makePublic
+                                           typedDef "GetAmountOfBullets" [] "int" (var "this.amounOfBullets"  |> ret) |> makePublic
+                                           TypedSigAbstract("Fire", [], "void") |> makePublic]))
       ]
-    SubSection ("Adding the instantiation logic to our new Animal")
-    ItemsBlock
-      [
-        ! @"Abstract classes provide an almost complete blueprint that cannot be instantiated directly"
-        ! @"Abstract classes can be instantiated only though its derived types"
 
-
-        ! @"For this reason we can only capture our instantiation logic through a new function \texttt{SelectNewAnimal} that returns a polymorphic type \texttt{Animal} and implements the above switch logic"
-        ! @"Note the function is static, since we cannot instantiate directly an object of type \textttt{Animal}"
-      ]
+    SubSection ("Instantiating abstract classes")
     ItemsBlock
       [
-        ! @"CODE"
-      ]    
+        ! @"Is not possible directly (what is the result of \texttt{new Weapon().Fire()}?)"
+        ! @"Abstract classes have to be inherited in order to use their functionalities"
+        ! @"All abstract methods must eventually come with an implementation"
+      ]
+    VerticalStack
+      [ 
+        ItemsBlockWithTitle "Implementing our weapon"
+          [
+            ! @"In the following a correct implementation of our \texttt{Weapon} is provided"
+          ]
+        CSharpCodeBlock(TextSize.Tiny, 
+                          (classDef "Gun" 
+                                    [extends "Weapon"
+                                     typedDefWithBase "Gun" ["int","amounOfBullets"] "" ["amounOfBullets"] endProgram |> makePublic
+                                     typedDef "Fire" [] "void" ("amounOfBullets" := Code.Op(var "amounOfBullets", Operator.Minus, ConstInt(-1)))|> makeOverride |> makePublic] >>
+                           classDef "FastGun" 
+                                    [extends "Weapon"
+                                     typedDefWithBase "Gun" ["int","amounOfBullets"] "" ["amounOfBullets"] endProgram |> makePublic
+                                     typedDef "Fire" [] "void" (("amounOfBullets" := Code.Op(var "amounOfBullets", Operator.Minus, ConstInt(-1))) >>
+                                                                ("amounOfBullets" := Code.Op(var "amounOfBullets", Operator.Minus, ConstInt(-1)))) |> makeOverride |> makePublic]))
+        
+      ]
+
+    SubSection ("Making our ``Animal'' abstract")
+    ItemsBlock
+      [
+        ! @"We can of course define our \texttt{Animal} abstract"
+        ! @"What follows?"
+        Pause
+        ! @"We can have a static method \texttt{SelectNewAnimal} that implements the instantiation mechanism, introduced at the beginning of this example, and returns a concrete animal"
+        ! @"We can have leave \texttt{MakeSound} as a signature"  
+        ! @"In the following we show our abstract \texttt{Animal} and we consume it"        
+      ]
+
+    CSharpCodeBlock(TextSize.Tiny, 
+                        (abstractClassDef "Animal" 
+                                          [typedDef "SelectNewAnimal" [] "Animal" 
+                                                     
+                                                             ((typedDeclAndInit "input" "int" (StaticMethodCall("Int32", "Parse", [(StaticMethodCall("Console", "ReadLine", []))]))) >>
+                                                              (Code.IfThen(Code.Op(var "input", Operator.Equals, constInt 1),
+                                                                            newC "Cat" [] |> ret)) >>
+                                                              (Code.IfThen(Code.Op(var "input", Operator.Equals, constInt 2),
+                                                                            newC "Dog" [] |> ret)) >>
+                                                              (Code.IfThen(Code.Op(var "input", Operator.Equals, constInt 3),
+                                                                            newC "Bird" [] |> ret)) >>
+                                                              (methodCall "this" "SelectNewAnimal" [] |> ret)) |> makePublic
+                                           TypedSigAbstract("MakeSound", [], "void") |> makePublic] >>
+                          dots >>
+                          (typedDeclAndInit "an_animal" "Animal" (staticMethodCall "Animal" "SelectNewAnimal" [])) >>
+                          (methodCall "an_animal" "MakeSound" [])))    
     SubSection "Consideration"
     ItemsBlock
       [
-        ! @"In the last version of our \texttt{Animal} class we managed to decouple the instantiation logic of general polymorphic types from the derived concrete classes"
-        ! @"The just described mechanism is commonly referred as factory design pattern"
+        ! @"In the last version of our \texttt{Animal} class we managed to define instantiation logic at polymorphic level, instead of carrying such task on all clients"
+        ! @"Now there is only one \textit{entry point} where we can create our concrete animals: in \texttt{Animal}!"
+        ! @"Whenever a client wishes to instantiate an animal it has to ask the permission to \texttt{Animal}"
+        ! @"We now can say that \texttt{Animal} is not only the polymorphic type for our concrete animals, but also a \textbf{factory} of animals"
+        ! @"This instantiation mechanism, which is recurrent in many domains, is commonly referred as factory design pattern"
+        ! @"More specifically, the just described mechanics is called \textit{simple factory method}"
       ]
 
-    Section "The factory design pattern"
-    SubSection "Formalization"
+    Section "The simple factory design pattern"
+    SubSection "Formalization"    
+    ItemsBlock
+      [
+        ! @"A simple factory is a method that called is directly from the client"
+        ! @"Such method returns one of many different classes, all implementing a parent class"
+        ! @"We could also include our simple factory method in this shared type, in this case it is reasonable to have such it \texttt{static}"
+      ]
+    UML
+        [ 
+          Package("Version 1",
+            [
+              Class("Client", -7.5, 0.0, Option.None, [], [])
+              Class("SimpleFactory", 0.0, 0.0, Option.None, [], [UMLItem.Operation("Create",["args"],Some "ParentClass")])
+              Class("ParentClass", 7.5, 0.0, Option.None, [], [])
+              Class("ChildClass1", -5.5, -3.0, Some "ParentClass", [], [])
+              Class("ChildClass2", 0.0, -3.0, Some "ParentClass", [], [])
+              Class("ChildClassN", 5.5, -3.0, Some "ParentClass", [], [])
+              Aggregation("Client","calls",Option.None,"SimpleFactory")
+              Aggregation("SimpleFactory","returns",Option.None,"ParentClass")
+            ])
+        ]
+    UML
+        [ 
+          Package("Version 2",
+            [
+              Class("Client", -6.5, 0.0, Option.None, [], [])
+              Class("ParentClass", 1.0, 0.0, Option.None, [], [UMLItem.Operation("Create",["args"],Some "ParentClass")])
+              Class("ChildClass1", -5.5, -3.0, Some "ParentClass", [], [])
+              Class("ChildClass2", 0.0, -3.0, Some "ParentClass", [], [])
+              Class("ChildClassN", 5.5, -3.0, Some "ParentClass", [], [])
+              Aggregation("Client","calls",Option.None,"ParentClass")
+            ])
+        ]
 
-    Section "The abstract factory"
     SubSection "Static methods are not enough"    
-    SubSection "We wish to have different classes defining instantiation mechanism (each for a different context) for the same polymorphic type"
-    SubSection "Formalization"
+    ItemsBlock
+      [
+        ! @"However static methods, and in general simple factories, are not enough"
+        ! @"What if we want to make the instantiation as well custom"
+        ! @"Static methods cannot be overrided!"
+      ]
+    ItemsBlock
+      [
+        ! @"A solution would be that our simple factory method becomes virtual"
+        ! @"Depending on the domain, a ``concrete factory'' is then selected by the client that implements such virtual methods"
+        ! @"This mechanism of \textit{interchangeable} factories is called the \texttt{factory method}"
+      ]    
+    Section "The factory method"
+    SubSection "Formalization"    
+    ItemsBlock
+      [
+        ! @"A factory method is: ``a class which defers instantiation of an object to subclasses''"
+        ! @"This is possible by means of abstract class"
+        ! @"By becoming abstract our factory method become virtual, which means that a client who wants to consume it should first provide a concrete class implementing such abstract factory"
+        ! @"More formally given an abstract factory class \texttt{A}, which contains a virtual method \texttt{Create}, and a series of classes \texttt{B1,..,Bn} all implementing a polymorphic type \texttt{P}"
+      ]
+    ItemsBlock
+      [
+        ! @"\texttt{Create} returns an object of type \texttt{P}"
+        ! @"Our client in order to instantiate an concrete \texttt{P} needs a concrete class \texttt{C} implementing our factory \texttt{A}"
+        ! @"\texttt{C} will be a special class that implements, according to some criteria, the virtual \texttt{Create}"
+      ]
+    ItemsBlock
+      [
+        ! @"By deferring instantiation of an object to subclasses a new client that has different criteria on mind for instantiating concrete \texttt{P}'s will provide a different concrete factory without changing the already existing relations"
+        ! @"Exchanging concrete factories does not affect other classes structures or behaviors"        
+      ]
 
+    UML
+        [ 
+            Class("Client", -7.5, 0.0, Option.None, [], [])
+            Class("Factory", -7.5, -2.0, Option.None, [], [UMLItem.Operation("Create",["args"],Some "ParentClass")])
+            Class("Concrete1", -7.5, -5.0, Some "Factory", [], [])
+            Class("Concrete2", -2.0, -5.0, Some "Factory", [], [])
+            Class("ConcreteM", 3.5, -5.0, Some "Factory", [], [])
+
+            Aggregation("Client","calls",Option.None,"Factory")
+
+            Class("ParentClass", 5.5, -2.0, Option.None, [], [])
+            Class("ChildClass1", -5.5, 3.0, Some "ParentClass", [], [])
+            Class("ChildClass2", 0.0, 3.0, Some "ParentClass", [], [])
+            Class("ChildClassN", 5.5, 3.0, Some "ParentClass", [], [])
+            Aggregation("Factory","returns",Option.None,"ParentClass")
+        ]
+
+    Section "The abstract factory method"
+    SubSection "Formalization"    
+    ItemsBlock
+      [
+       ! @"The biggest pattern of the factories seen so far"
+       ! @"Is acts the same as the factory method, except for the fact that it might contain more than one virtual instantiation method"
+       ! @"Each of them returning a different but related polymorphic object"
+      ]
+    UML
+        [ 
+            Class("Client", -7.5, 0.0, Option.None, [], [])
+            Class("Factory", -7.5, -2.0, Option.None, [], [UMLItem.Operation("Create1",["args"],Some "ParentClass1")
+                                                           UMLItem.Operation("Create2",["args"],Some "ParentClass2")])
+            Class("Concrete1", -7.5, -5.0, Some "Factory", [], [])
+            Class("Concrete2", -2.0, -5.0, Some "Factory", [], [])
+            Class("ConcreteM", 3.5, -5.0, Some "Factory", [], [])
+
+            Aggregation("Client","calls",Option.None,"Factory")
+
+            Class("ParentClass1", 5.5, 1.0, Option.None, [], [])
+            Class("ChildClass11", 0.0, 3.0, Some "ParentClass1", [], [])
+            Class("ChildClass1N", 5.5, 3.0, Some "ParentClass1", [], [])
+
+            Class("ParentClass2", 5.5, 0.0, Option.None, [], [])
+            Class("ChildClass21", 0.0, -2.0, Some "ParentClass2", [], [])
+            Class("ChildClass2N", 5.5, -2.0, Some "ParentClass2", [], [])
+
+            Aggregation("Factory","returns",Option.None,"ParentClass1")
+            Aggregation("Factory","returns",Option.None,"ParentClass2")
+        ]
     SubSection "Conclusions"
     ItemsBlock
       [
-        ! @"Conclusions"
+        ! @"Sometime we need interfaces to implement virtual constructor"
+        ! @"Why? Because sometimes we know the polymorphic type to instantiate first and later the concrete one"
+        ! @"A naive solution would see the client code implement such instantiation mechanism, but this is will yield to repetition and would make the code not maintainable"
+        ! @"Factories solve such issue elegantly by promoting virtual constructors, by means of abstract classes, or via static methods"
+      ]
+    ItemsBlock
+      [
+        ! @"Static methods are less flexible when compared to abstract classes, since abstract classes allow both virtual and not virtual methods"
+        ! @"Moreover, abstract classes allow the definition of multiple interchangeable concrete factories, each shaped for a specific domain"
       ]
   ]
 
--- Slides/Program.fs
+++ Slides/Program.fs
@@ -3,8 +3,8 @@
 let makeSlides (title : string) =
 //  do batchProcess LatexDefinition.generatePresentation (Chapter1.Week1.v2.slides title) (sprintf "%s_Lec1_DP_intro" title) "The INFDEV team" "Introduction" false true
 //  do batchProcess LatexDefinition.generatePresentation (Chapter2.Week2.v1.slides title) (sprintf "%s_Lec2_DP_iterator" title) "The INFDEV team" "Iterating collections" false true
-  do batchProcess LatexDefinition.generatePresentation (Chapter3.Week3.v1.slides title) (sprintf "%s_Lec3_DP_adapter" title) "The INFDEV team" "Adapting interfaces" true true
-//  do batchProcess LatexDefinition.generatePresentation (Chapter4.Week4.v1.slides title) (sprintf "%s_Lec3_DP_factory" title) "The INFDEV team" "Allowing virtual constructors" true false
+//  do batchProcess LatexDefinition.generatePresentation (Chapter3.Week3.v1.slides title) (sprintf "%s_Lec3_DP_adapter" title) "The INFDEV team" "Adapting interfaces" true true
+  do batchProcess LatexDefinition.generatePresentation (Chapter4.Week4.v1.slides title) (sprintf "%s_Lec4_DP_factory" title) "The INFDEV team" "Allowing virtual constructors" true false
 
 
 // REMOVE UNREPEATED EVERYWHERE IT WORKS!!!!!!!!! FREE SLIDEZ!!!!1111oneoneone
